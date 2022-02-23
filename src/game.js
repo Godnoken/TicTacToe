86d2ac54837068cc3a055e2a45d7c68a389973ce
Sebastian Ojefors <godnoken@gmail.com>
2022-02-24 00:59:51 +0100

    Add win and draw conditions logic

--- src/game.js
+++ src/game.js
@@ -10,23 +10,44 @@ const Player = (name, marker) => {
 
 
 const gameBoard = (() => {
-    const gameBoardArray = [
+    let gameBoardArray = [
         "", "", "",
         "", "", "",
         "", "", ""
     ];
 
+    const resetGameBoardArray = () => {
+        for (let i = 0; i < gameBoardArray.length; i++) {
+            gameBoardArray[i] = "";
+        }
+    }
+
     return {
         gameBoardArray,
+        resetGameBoardArray
     }
 })();
 
 
 const displayController = (() => {
+    const gameBoardContainer = document.querySelector(".gameBoard");
 
+    for (i = 0; i < gameBoard.gameBoardArray.length; i++) {
+        const gameBoardTile = document.createElement("div");
+        gameBoardTile.classList.add("gameBoardTile");
+        gameBoardTile.dataset.index = i;
+        gameBoardTile.textContent = gameBoard.gameBoardArray[i];
+        gameBoardContainer.appendChild(gameBoardTile);
+    }
+
+    const resetGameBoardVisuals = () => {
+        for (i = 0; i < gameBoard.gameBoardArray.length; i++) {
+            gameBoardContainer.children[i].textContent = "";
+        }
+    }
 
     return {
-
+        resetGameBoardVisuals,
     }
 })();
 
@@ -37,45 +58,74 @@ const gameController = (() => {
     const player1 = Player("TicToe", "X")
     const player2 = Player("TacToe", "O")
     let round = 1;
-    let gameOver = false;
-    let currentPlayer = player1.getName();
-    
-    const getCurrentPlayerName = () => {
-        return round % 2 === 1 ? player1.getName() : player2.getName();
-    };
+    let currentPlayer = player1;
 
     gameBoardContainer.addEventListener("click", (event) => {
         const tile = event.target;
 
         if (tile.textContent !== "O" && tile.textContent !== "X") {
 
-            if (currentPlayer === player1.getName()) {
+            if (currentPlayer.getName() === player1.getName()) {
                 tile.textContent = player1.getMarker();
+                gameBoard.gameBoardArray[tile.dataset.index] = player1.getMarker();
             }
             else {
                 tile.textContent = player2.getMarker();
+                gameBoard.gameBoardArray[tile.dataset.index] = player2.getMarker();
             }
 
-            round++;
-            currentPlayer = getCurrentPlayerName();
+            if (isGameOver()) resetGame();
+            else {
+                round++;
+                console.log(round)
+                currentPlayer = getCurrentPlayer();
+            }
         }
     })
 
+    const getCurrentPlayer = () => {
+        return round % 2 === 1 ? player1 : player2;
+    };
+
+    const isGameOver = () => {
+        if (round === 9 || checkWinner(currentPlayer.getMarker())) {
+            return true;
+        }
+    }
+
+    const resetGame = () => {
+        round = 1;
+        displayController.resetGameBoardVisuals();
+        gameBoard.resetGameBoardArray();
+    }
+
+    const checkWinner = (mark) => {
+        const GBA = gameBoard.gameBoardArray;
+
+        const winConditions = (
+            GBA[0] === mark && GBA[1]  === mark && GBA[2]  === mark
+            ||
+            GBA[3] === mark && GBA[4]  === mark && GBA[5]  === mark
+            ||
+            GBA[6] === mark && GBA[7]  === mark && GBA[8]  === mark
+            ||
+            GBA[0] === mark && GBA[3]  === mark && GBA[6]  === mark
+            ||
+            GBA[1] === mark && GBA[4]  === mark && GBA[7]  === mark
+            ||
+            GBA[2] === mark && GBA[5]  === mark && GBA[8]  === mark
+            ||
+            GBA[0] === mark && GBA[4]  === mark && GBA[8]  === mark
+            ||
+            GBA[2] === mark && GBA[4]  === mark && GBA[6]  === mark
+        );
+
+        if (winConditions) {
+            return true;
+        }
+    }
 
     return {
         
     }
-})();
-
-
-const renderGameBoard = (() => {
-    const gameBoardContainer = document.querySelector(".gameBoard");
-
-    for (i = 0; i < gameBoard.gameBoardArray.length; i++) {
-        const gameBoardTile = document.createElement("div");
-        gameBoardTile.classList.add("gameBoardTile");
-        gameBoardTile.textContent = gameBoard.gameBoardArray[i];
-        gameBoardContainer.appendChild(gameBoardTile);
-    }
-
 })();
